4ee00642e499fccb19a250c8b390e6e17aa91b35
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-01-24 20:38:42 +0100

    Added - CORS, staticFiles, bodyParser and server middlewares to container.

---
+++ server/middlewares/index.js
@@ -0,0 +1,6 @@
+import cors from "./cors.js";
+import staticFiles from "./staticFiles.js";
+import bodyParser from "./bodyParser.js";
+import server from "./server.js";
+
+export {cors, staticFiles, bodyParser, server};
