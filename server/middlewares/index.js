9c435d6e681ce247f384000cbc2926e37a0ff00e
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-02-20 11:34:02 +0100

    - Added - routes middleware container.

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,6 +1,7 @@
 import cors from "./cors.js";
 import staticFiles from "./staticFiles.js";
 import bodyParser from "./bodyParser.js";
+import routes from "./routes.js";
 import server from "./server.js";
 
-export {cors, staticFiles, bodyParser, server};
+export {cors, staticFiles, bodyParser,routes, server};
